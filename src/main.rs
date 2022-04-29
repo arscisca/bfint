8820d597de691655554c171cedcc837133164cc0
Alessandro Rocco Scisca <ale.scisca2@gmail.com>
2022-04-29 20:56:03 +0100

    Implement main behavior

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
 mod interpreter;
 
+extern crate argparse;
+
+use argparse::ArgumentParser;
+use std::error::Error;
+
 use interpreter::interpreter::Interpreter;
-fn main() {
-    let mut interpreter = Interpreter::interpret_file("test/helloworld.bf").expect("Could not interpret file");
-    interpreter.dump_program(&mut std::io::stdout());
-    // interpreter.run();
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut fname = String::new();
+    let mut memsize = 4096;
+    {
+        // Parse args
+        let mut parser = ArgumentParser::new();
+        parser.set_description("An over-engineered brainf*ck interpreter.");
+
+        parser.refer(&mut fname)
+            .add_argument("fname", argparse::Store, "brainf*ck file to run");
+
+        parser.refer(&mut memsize)
+            .add_option(&["--memsize"], argparse::Store, "amount of memory to allocate in bytes");
+
+        if let Err(code) = parser.parse_args() {
+            return Err(format!("Error while parsing arguments: code {}", code).into());
+        }
+    }
+    // Run interpreter
+    if fname.is_empty() {
+        // CL mode
+        todo!("Command line mode is not supported yet");
+    } else {
+        let mut interpreter = Interpreter::interpret_file(&fname)?.with_memory(memsize);
+        interpreter.run()?;
+    }
+    Ok(())
 }
